b9533d5cbe348bf1b666dc3d01a75636fcd09a05
Shivam sharma <112563462+Shivam8b@users.noreply.github.com>
2023-09-05 14:00:25 +0530

    sets
    basics of python

---
+++ sets.py
@@ -0,0 +1,21 @@
+s = {2, 4, 6, 7,}
+print (s)
+
+harry = set()
+print(type(harry))
+
+s1 = {12, 23, 2 ,55}
+s2 = {24, 55, 77, 2 , 12 }
+#print(s1.union(s2))#to add two sets by using union
+#s1.update(s2) #to add the numbers that are in s2 but not in s1
+# print(s1.intersection(s2))#gives values that are present in both the sets
+#print(s1.symmetric_difference(s2))#gives us values that are not same in bothe the list
+#print(s1.difference(s2))#check later
+#print(s2.intersection_update(s1))#Removes the items in this set that are not present in other, specified set(s)
+#print(s1.isdisjoint(s2)) #Returns whether two sets have a intersection or not (true\false)
+#print(s1.issuperset(s2))  #	Returns whether this set contains another set or not
+#s1.add(20)  #Adds an element to the set
+#s1.remove(20) #Removes all the elements from the set
+#discard()	Remove the specified item
+#pop() Removes an element from the set
+#del() it is not a method but it can remove the entire set 
