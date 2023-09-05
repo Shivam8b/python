b126b7d069aa6f238815dffd1d2ef1e90be7b52f
Shivam sharma <112563462+Shivam8b@users.noreply.github.com>
2023-09-05 14:02:35 +0530

    while
    Basic of python

---
+++ while.py
@@ -0,0 +1,100 @@
+#i = 0
+#while (i <= 3):
+    #print(i)
+    #i = i + 1
+#print("loop ends here" )
+
+
+
+#i = int(input("Enter the number: "))
+#while (i <=38):
+    #i = int(input("Enter the number: "))
+    #print(i)
+#print (" done with the loop")
+
+
+#count = 5 #decreasing value
+#while ( count > 0):
+    #print(count)
+    #count = count -1
+#else :
+    #print("i am inside else")
+
+
+
+'''
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+'''
+
+'''
+x = 1
+while x <= 20:
+  print(x)
+  x = x + 1
+'''
+
+'''
+i = 1
+while i <= 10:
+    print(i)
+    i += 1'''
+
+
+'''row  = 5
+for i in range(1, row + 1, 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print("")'''
+
+
+'''s = 0
+n = int(input("Enter number "))
+for i in range(1, n + 1, 1):
+    s += i
+print("\n")
+print("Sum is: ", s)'''
+
+'''n = 2
+for i in range(1, 11, 1):
+    product = n * i
+    print(product)'''
+
+
+'''numbers = [12, 75, 150, 180, 145, 525, 50]
+for item in numbers:
+    if item > 500:
+        break
+    elif item > 150:
+        continue
+    elif item % 5 == 0:
+        print(item)'''
+
+'''
+num = 75869
+count = 0
+while num != 0:
+    num = num // 10
+    count = count + 1
+print("Total digits are:", count)'''
+
+'''
+numbers = [12, 75, 150, 180, 145, 525, 50]
+for item in numbers:
+    if item > 500:
+        break
+    elif item > 150:
+        continue
+    elif item % 5 == 0:
+        print(item'''
+
+'''num = 75869
+count = 0
+while num != 0:
+    num = num // 10
+    count = count + 1
+print("Total digits are:", count)'''
