e2ea37ee643eeb8bd0c21260bc0bc95b9a64fc81
Shivam8b <112563462+Shivam8b@users.noreply.github.com>
2023-09-05 13:49:28 +0530

    dictianary
    Basic soof dictionary

---
+++ dict.py
@@ -0,0 +1,28 @@
+'''dic = {
+    "Harry" : "Human being",
+    "Bat" : "man"
+}'''
+#print(dic["Bat"])
+#print(dic.get("Bat")) when key does not exist
+#print(dic.keys()) to display all keys
+#print(dic.values()) #to get all the values
+
+'''for key in dic.keys():  #to get keys from the dictionary 
+    print(dic[key])'''
+
+'''for key in dic.keys():
+    print(f"The vlaue corresponding to the key {key} is {dic[key]}")'''
+
+
+name = { 1 : "shubham ", 2 : "shivam", 3 : "Dinesh",  4 : "Abishek"
+}
+'''for x in name.keys():
+    print(f"The name of {x} student is {name[x]}") # to print the values with keys in order '''
+
+'''print(name.items())#it gives the display of whole dictionary
+for key, value in name.items():
+    print(f"The name of {key} student is {value}") #to print the dictory by using the item keyword and with f string'''
+
+dic.name.update
+
+
